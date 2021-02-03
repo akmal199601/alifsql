2912ea17a25b82ad39e7f7548a3948308bda9897
akmal199601 <62462485+akmal199601@users.noreply.github.com>
2021-02-03 18:04:46 +0500

    its me home work

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+use alif
+--SELECT * FROM alifdemy;
+--UPDATE alifdemy
+--SET address = age, id = address
+--INSERT INTO alifdemy(name, address, id)
+--VALUES ('1', 'akmal', '20','adres','2')
+--delete alifdemy where Id = 1
